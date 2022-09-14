b3e16dfcd82cde1c103b89d108f62e6391634747
Tim Kelley <timkelley683@gmail.com>
2022-09-14 16:29:46 -0700

    tests for appropriate files

---
+++ src/box.test.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import Box from './Box';
+
+test('renders without crashing', () => {
+  render(<Box />);
+  
+});
+
+it('matches snapshot', function () {
+  const { asFragment } = render(<Box />);
+  expect(asFragment()).toMatchSnapshot();
+})
